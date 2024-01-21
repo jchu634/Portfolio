d00ac1a1e145d18e36ab2caa171d0e9fb16355a3
JCHU634 <87676426+jchu634@users.noreply.github.com>
2024-01-21 19:58:11 +1300

    Updated project details

--- frontend/app/projects/page.tsx
+++ frontend/app/projects/page.tsx
@@ -5,6 +5,17 @@ import {
     AccordionTrigger,
   } from "@/components/ui/accordion"
 
+import Link from 'next/link';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons"
+
+import {
+  faArrowUpRightFromSquare
+} from "@fortawesome/free-solid-svg-icons"
+
 const projects = [
   { 
     name: 'CodeCritters', type: 'Hybrid Application and Website', 
@@ -20,15 +31,21 @@ const projects = [
     website:'https://codecritters.live/'
   },
   {
-    name: 'CodeCritters Flutter App', type: 'application',
-    description: 'Lorem Ipsum',
+    name: 'CodeCritters Flutter App', type: 'Application',
     technologies_and_frameworks: ['Flutter', 'Dart'],
+    description: 'This is an sister android camera application for CodeCritters which allows users to take photos and get inference results via a local TensorFlowLite model.',
+    github:'https://github.com/uoa-compsci399-s2-2023/capstone-project-team-34-code-critters-flutterapp',
   },
   {
-    name: 'Fakman', type: 'application', technologies_and_framework:['Unity', 'C#'],  description: 'Lorem Ipsum'
+    name: 'Fakman', type: 'Game',
+    technologies_and_frameworks:['Unity', 'C#'], 
+    description: 'This is a Pacman clone made as a learning project for Unity and C#.',
+    github:'https://github.com/jchu634/fakman',
   },
   { 
-    name: 'Portfolio', type: 'website', technologies_and_frameworks:['Next.JS', 'TailwindCSS'], description: 'Lorem Ipsum'
+    name: 'Portfolio', type: 'Website',
+    technologies_and_frameworks:['Next.JS', 'TailwindCSS'],
+    description: 'Lorem Ipsum'
   },
 ];
 function exportProjectsAsString(technologies_and_frameworks: string[]){
@@ -62,7 +79,29 @@ export default function Page(){
                         <>{exportProjectsAsString(project.technologies_and_frameworks)}</>
                       )
                     }
-                    </p><br></br>
+                    </p>
+                    <div className="flex flex-row items-center justify-start space-x-2">
+                      {
+                      project.github && (
+                        <Link
+                        href={project.github}
+                        >
+                          <FontAwesomeIcon icon={faGithub} size="2x"/>
+                        </Link>
+                        
+                      )
+                    }
+                    {
+                      project.website && (
+                        <Link
+                        href={project.website}
+                        >
+                          <FontAwesomeIcon icon={faArrowUpRightFromSquare} size="2x"/>
+                        </Link>
+                      )
+                    }
+                    </div>
+                    
                     <p className="text-indigo-950 dark:text-sky-50 whitespace-pre-wrap"><b>Project Description: </b> {project.description}</p>
                   </AccordionContent>
                 </AccordionItem>
