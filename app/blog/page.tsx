cf4eda876ab0b55e5811f2958552edf29a88c192
JCHU634 <87676426+jchu634@users.noreply.github.com>
2024-01-25 01:37:33 +1300

    Added auto-links to blog page, added test article

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,5 +1,6 @@
 import { getSortedPostsData, getAllPostNames } from '@/lib/post';
 import { Separator } from '@/components/ui/separator'
+import Link from 'next/link';
 
 export default function Page(){
     const allPostsData = getSortedPostsData();
@@ -9,15 +10,19 @@ export default function Page(){
             <h1 className="text-3xl font-bold text-blue-900 dark:text-slate-200">Blog</h1><br></br>
             <ul>
                 { allPostsData.map(({ id, date, title, description }) => (
-                <div className="mb-4" >
-                    <li key={id} className="mb-4">
-                        <p>{id}</p>
-                        <h2 className="text-2xl font-bold">{title.toString()}</h2>
-                        <p className="text-base">{description.toString()}</p>
-                        <p className="text-base">Published: {date.toString()}</p>
-                    </li>
-                    <Separator></Separator>
-                </div>
+                    <Link
+                    href={`/blog/${id}`}>
+                        <div className="mb-4" >
+                            <li key={id} className="mb-4">
+                                {/* <p>{id}</p> */}
+                                <h2 className="text-2xl font-bold">{title.toString()}</h2>
+                                <p className="text-base">{description.toString()}</p>
+                                <p className="text-base">Published: {date.toString()}</p>
+                            </li>
+                            <Separator></Separator>
+                        </div>
+                    </Link>
+                
                 
                 ))}
                 
@@ -25,6 +30,4 @@ export default function Page(){
             
         </div>
     )
-    
-    // return <p> Blogs Page (In Construction)</p>
 }
