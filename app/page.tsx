840c21412fc6bdc410130a60e1c20b0857277482
JCHU634 <87676426+jchu634@users.noreply.github.com>
2024-02-01 01:49:29 +1300

    Finalised main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,24 @@
 "use client"
 import React from 'react';
-import CustomCursorTrail from '@/components/cursorTrail'
-import CustomCursorGif from '@/components/cursorGif'
+import CustomCursorTrail from '@/components/cursorTrail';
+import CustomCursorGif from '@/components/cursorGif';
 
-import { Card, CardContent } from "@/components/ui/card"
-import { Button } from "@/components/ui/button"
-import Image from "next/image"
-import Link from "next/link"
+import { Card, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import {Alert, AlertTitle, AlertDescription} from "@/components/ui/alert";
+import Image from "next/image";
+import Link from "next/link";
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from "@/components/ui/carousel"
+} from "@/components/ui/carousel";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faArrowUpRightFromSquare, faEnvelopeSquare } from '@fortawesome/free-solid-svg-icons';
 
 const projects = [
   { 
@@ -76,23 +77,17 @@ const projects = [
     github:'https://github.com/jchu634/portfolio',
     link:'/',
     description: 'This very website you are on right now!\nThis website is built using Next.JS and TailwindCSS.',
-  },
-  { 
-    name: 'More Projects', type: 'Website',
-    link:'/projects',
-    description: 'Click on me to find more projects.',
-  },
+  }
 ];
 
 function mapProjects(project:any, index:number){
   return (
     <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
       <div className="p-1">
-          <Card className="group">
-          <CardContent className="relative flex aspect-square items-center justify-center p-6 group-hover:bg-opacity-50 dark:group-hover:bg-opacity-20 dark:group-hover:bg-gray-100 group-hover:bg-gray-800">
+          <Card className="group bg-slate-200 dark:bg-sky-950 dark:bg-opacity-40">
+            <CardContent className="relative flex aspect-square items-center justify-center p-6 group-hover:bg-opacity-50 dark:group-hover:bg-opacity-20 dark:group-hover:bg-gray-100 group-hover:bg-gray-800">
               <div className="flex flex-col gap-y-2">
                 <span className="text-2xl font-semibold">{project.name}</span>
-                
                 {                
                   project.image && (
                     <div className="group-hover:filter dark:group-hover:grayscale-0 group-hover:brightness-50">
@@ -102,17 +97,14 @@ function mapProjects(project:any, index:number){
                         width={200}
                         height={200}
                         alt={project.image_alt}
-                        >
-
-                        </Image>
+                      ></Image>
                       <Image
                       className="block dark:hidden max-h-52 w-auto"
                         src={project.image_light}
                         width={200}
                         height={200}
                         alt={project.image_alt}
-                        >
-                        </Image>
+                      ></Image>
                     </div>
                   )
                 }
@@ -121,24 +113,17 @@ function mapProjects(project:any, index:number){
               <div className="hidden absolute inset-0 group-hover:flex items-center justify-center gap-x-2">
               {                
                   project.github && (
-                    <Link
-                    href={project.github}
-                    
-                    >
-                      <Button className="dark:bg-gray-100">
+                    <Link href={project.github}>
+                      <Button className="bg-gray-700 dark:bg-gray-100">
                           <FontAwesomeIcon icon={faGithub} size="2x"/>
                       </Button>
-                    </Link>
-                    
+                    </Link> 
                   )
                 } 
-                <Link
-                href={project.link}
-                >
-                 <Button className="dark:bg-gray-100">
+                <Link href={project.link}>
+                 <Button className="bg-gray-700 dark:bg-gray-100">
                     <FontAwesomeIcon icon={faArrowUpRightFromSquare} size="2x"/>
                   </Button> 
-
                 </Link>
               </div>
             </CardContent>
@@ -153,23 +138,66 @@ export default function Home() {
     <main>
       <CustomCursorTrail />
       <CustomCursorGif />
-      <h1 className="text-3xl font-bold text-blue-900 dark:text-slate-200">Hi,</h1><br></br>
-      <h1 className="text-2xl font-bold text-blue-900 dark:text-slate-200">I&apos;m Joshua, a FullStack Developer who prefers backend development</h1><br></br>
+      <h1 className="text-3xl font-bold text-black dark:text-slate-200">Hi, I&apos;m Joshua</h1><br></br>
+      <h2 className="text-2xl font-bold text-black dark:text-slate-200">About</h2><br></br>
+      <ul className="pl-5 text-xl text-black dark:text-slate-200 whitespace-pre-wrap list-disc">
+        <li>
+          Final year Computer Science Student at the University of Auckland.<br />
+        </li>
+        <li>
+          FullStack Developer who prefers backend development
+        </li>
+      </ul><br/>
+      <h2 className="text-2xl font-bold text-black dark:text-slate-200">Contact</h2><br></br>
+      <div className="flex flex-col md:flex-row justify-start gap-3">
+        <Link href="https://github.com/jchu634">
+          <Button className="h-12 gap-2 bg-blue-600 dark:hover:bg-sky-900">
+            <FontAwesomeIcon className="dark:text-slate-200" icon={faGithubSquare} size="3x"/>
+            <p className="text-lg dark:text-slate-200">@jchu634</p>
+          </Button>
+        </Link>
+        <Link href="https://www.linkedin.com/in/jchu634/">
+          <Button className="h-12 gap-2 bg-blue-600 dark:hover:bg-sky-900">
+            <FontAwesomeIcon className="dark:text-slate-200" icon={faLinkedin} size="3x"/>
+            <p className="text-lg dark:text-slate-200">@jchu634</p>
+          </Button>
+        </Link>
+        <Link href="mailto:jchu634@keshuac.com">
+          <Button className="h-12 gap-2 bg-blue-600 dark:hover:bg-sky-900">
+            <FontAwesomeIcon className="dark:text-slate-200" icon={faEnvelopeSquare} size="3x"/>
+            <p className="text-lg dark:text-slate-200">jchu634@keshuac.com</p>
+          </Button>
+        </Link>
+      </div><br/>
+      <h1 className="text-2xl font-bold text-black dark:text-slate-200">Here are a couple of projects I worked on:</h1><br></br>
       <Carousel
         opts={{
           align: "start",
+          loop: true,
         }}
-        className="w-full sm:max-w-screen-xs lg:max-w-screen-lg"
+        className="w-full sm:max-w-screen-xs lg:max-w-screen-lg xl:max-w-screen-xl"
       >
         <CarouselContent>
           { projects.map((project, index) => {
             return (mapProjects(project, index));
           })}
-          mapProjects(Projects)
         </CarouselContent>
         <CarouselPrevious />
         <CarouselNext />
       </Carousel>
+      <Alert className="w-60">
+        <AlertTitle>More Projects:</AlertTitle>
+        <div className="flex flex-row items-center gap-4">
+          <AlertDescription>
+            See more projects at:
+          </AlertDescription>
+          <Link href="/projects">
+            <Button className="w-10 h-10 bg-blue-600 dark:hover:bg-sky-900">
+                <FontAwesomeIcon className="dark:text-slate-200" icon={faArrowUpRightFromSquare} size="2x"/>
+            </Button>
+          </Link>
+        </div>
+      </Alert>
     </main>
   )
 }
