29762202f435d743a19494b7b03f2f747343926d
JCHU634 <87676426+jchu634@users.noreply.github.com>
2024-02-02 03:07:13 +1300

    Added Carousel of images to projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,20 +1,27 @@
 import {
-    Accordion,
-    AccordionContent,
-    AccordionItem,
-    AccordionTrigger,
-  } from "@/components/ui/accordion"
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+import { Card, CardContent } from "@/components/ui/card";
 
 import Link from 'next/link';
+import Image from 'next/image';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-  faGithub,
-} from "@fortawesome/free-brands-svg-icons"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, } from "@fortawesome/free-brands-svg-icons";
 
-import {
-  faArrowUpRightFromSquare
-} from "@fortawesome/free-solid-svg-icons"
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 
 const projects = [
   { 
@@ -28,7 +35,13 @@ const projects = [
     "The local app bundles the website and backend using PyWebView, compiles it via PyInstaller and finally bundles it using InnoSetup.",
 
     github:'https://github.com/uoa-compsci399-s2-2023/capstone-project-team-34-code-critters',
-    website:'https://codecritters.live/'
+    website:'https://codecritters.live/',
+    images: [
+      '/projects/codecritters/codecritters_light.png','/projects/codecritters/codecritters_dark.png',
+      '/projects/codecritters/codecritters_upload_light.png','/projects/codecritters/codecritters_upload_dark.png',
+      '/projects/codecritters/codecritters_predictions_light.png','/projects/codecritters/codecritters_predictions_dark.png',
+      '/projects/codecritters/codecritters_predictions_about_light.png','/projects/codecritters/codecritters_predictions_about_dark.png',
+    ]
   },
   {
     name: 'CodeCritters Flutter App', type: 'Application',
@@ -36,6 +49,7 @@ const projects = [
     description: 'This is an sister android camera application for CodeCritters which allows users to take photos and get inference results via a local TensorFlowLite model.\n'+
     "This app is fully functional, and although it was never publicly visible on the Google Play Store, it is still available for download via Github releases.",
     github:'https://github.com/uoa-compsci399-s2-2023/capstone-project-team-34-code-critters-flutterapp',
+    images:['/projects/codecrittersapp/codecrittersapp.jpg','/projects/codecrittersapp/codecrittersapp_prediction.png']
   },
   {
     name: 'Fakman', type: 'Game',
@@ -44,6 +58,7 @@ const projects = [
     "This game has a playable windows build and a webGL build which can be played from the link above.",
     github:'https://github.com/jchu634/fakman',
     website:"/projects/games/Fakman",
+    images:['/projects/fakman/fakman.png']
   },
   {
     name:'Loopy-Desktop',
@@ -52,7 +67,8 @@ const projects = [
     description:
       'This is a Electron wrapper for Loopy, an open source web application which allows users to visualise systems.\n'+
       'Additionally, this fork adds the ability to create and save GIFs of the visualisations.',
-    github:'https://github.com/jchu634/loopy-desktop'
+    github:'https://github.com/jchu634/loopy-desktop',
+    images:['/projects/loopy/Loopy.png', '/projects/loopy/loopy_output.gif','/projects/loopy/loopy_example.png']
   },
   { 
     name: 'Portfolio Website', type: 'Website',
@@ -99,37 +115,81 @@ function mapProject(project:any, index:number){
     <Accordion type="single" key={`${project.name}`} collapsible>
       <AccordionItem value={`item-${index + 1}`}>
         <AccordionTrigger> <p className="font-bold text-blue-900 dark:text-slate-200">{project.name}</p></AccordionTrigger>
-        <AccordionContent>
-          <p className="text-indigo-950 dark:text-sky-50"><b>Project Type: </b>{project.type}</p>
-          <p className="text-indigo-950 dark:text-sky-50"><b>Project Technologies: </b>
-          {
-            project.technologies_and_frameworks && (
-              <>{exportProjectsAsString(project.technologies_and_frameworks)}</>
-            )
-          }
-          </p>
-          <div className="flex flex-row items-center justify-start space-x-2">
+        <AccordionContent className="flex flex-col lg:flex-row justify-between last:md:pr-10">
+          <div>
+            <p className="text-indigo-950 dark:text-sky-50"><b>Project Type: </b>{project.type}</p>
+            <p className="text-indigo-950 dark:text-sky-50"><b>Project Technologies: </b>
             {
-            project.github && (
-              <Link
-              href={project.github}
-              >
-                <FontAwesomeIcon icon={faGithub} size="2x"/>
-              </Link>
-              
-            )
-          }
-          {
-            project.website && (
-              <Link
-              href={project.website}
-              >
-                <FontAwesomeIcon icon={faArrowUpRightFromSquare} size="2x"/>
-              </Link>
-            )
-          }
+              project.technologies_and_frameworks && (
+                <>{exportProjectsAsString(project.technologies_and_frameworks)}</>
+              )
+            }
+            </p>
+            <div className="flex flex-row items-center justify-start space-x-2">
+              {
+              project.github && (
+                <Link
+                href={project.github}
+                >
+                  <FontAwesomeIcon icon={faGithub} size="2x"/>
+                </Link>
+            
+              )
+            }
+            {
+              project.website && (
+                <Link
+                href={project.website}
+                >
+                  <FontAwesomeIcon icon={faArrowUpRightFromSquare} size="2x"/>
+                </Link>
+              )
+            }
+            </div>
+            <p className="text-indigo-950 dark:text-sky-50 whitespace-pre-wrap"><b>Project Description: </b> {project.description}</p>
           </div>
-          <p className="text-indigo-950 dark:text-sky-50 whitespace-pre-wrap"><b>Project Description: </b> {project.description}</p>
+
+          {
+            project.images && (
+            <div className="lg:w-1/3 md:pl-10">
+              <Carousel
+                opts={{
+                  align: "start",
+                  loop: true,
+                }}
+                className="w-full sm:max-w-fit lg:max-w-fit xl:max-w-fit max-h-fit "
+              >
+              <CarouselContent>
+                {project.images.map((images:any,index:number) => {
+                  return (
+                  <CarouselItem key={index}>
+                    <div className="p-1">
+                      <Card className="bg-slate-200 dark:bg-sky-950 dark:bg-opacity-40">
+                        <CardContent className="flex p-6 justify-center">
+                          <Image
+                            src={images}
+                            width={400}
+                            height={400}
+                            alt="CodeCritters Homepage"
+                          />
+                        </CardContent>
+                      </Card>
+                    </div>
+                  </CarouselItem>
+                  )
+                  })
+                }
+              
+              </CarouselContent>
+              
+                <CarouselPrevious />
+                <CarouselNext />
+              </Carousel>
+            </div>
+            )
+          }
+          
+
         </AccordionContent>
       </AccordionItem>
     </Accordion>
