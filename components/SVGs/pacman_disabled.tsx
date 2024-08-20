99240ae7757569b72f2d9d05366f51a5a499a13a
JCHU634 <87676426+jchu634@users.noreply.github.com>
2024-08-20 22:59:30 +1200

    added pacman enable and disable icons

---
+++ components/SVGs/pacman_disabled.tsx
@@ -0,0 +1,27 @@
+import * as React from "react"
+import { SVGProps } from "react"
+
+const PacmanDisabledSVG = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={541.6}
+    height={571.11}
+    viewBox = "0 0 541.6 571.11"
+    {...props}
+  >
+    <path
+      d="M535.441 412.339a280.868 280.868 0 1 1 .745-250.606L284.493 286.29Z"
+      style={{
+        fill: "#000",
+      }}
+    />
+    <path
+      d="M541.6 0 0 571.11"
+      style={{
+        stroke: "red",
+        strokeWidth: 75,
+      }}
+    />
+  </svg>
+)
+export default PacmanDisabledSVG
---
+++ components/SVGs/pacman_enabled.tsx
@@ -0,0 +1,19 @@
+import * as React from "react"
+import { SVGProps } from "react"
+const PacmanEnabledSVG = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={541.6}
+    height={571.11}
+    viewBox = "0 0 541.6 571.11"
+    {...props}
+  >
+    <path
+      d="M535.441 412.339a280.868 280.868 0 1 1 .745-250.606L284.493 286.29Z"
+      style={{
+        fill: "#000",
+      }}
+    />
+  </svg>
+)
+export default PacmanEnabledSVG
