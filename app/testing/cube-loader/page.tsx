7bc0785a2795daeaafb6ede5c1de25658d60411b
JCHU634 <87676426+jchu634@users.noreply.github.com>
2024-10-29 00:08:27 +1300

    Added Model import experiment

---
+++ app/testing/cube-loader/page.tsx
@@ -0,0 +1,54 @@
+"use client";
+import React, { Suspense, useRef, useEffect } from 'react';
+import { useTheme } from 'next-themes';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Mesh, Group } from 'three';
+import { useGLTF } from '@react-three/drei';
+
+function Box(props: JSX.IntrinsicElements['mesh']){
+    const ref = useRef<Mesh>(null!);
+    
+    const { nodes, materials } = useGLTF('/cube.gltf');
+    return (
+        <primitive object={nodes.Cube} ref={ref} {...props} />
+    )
+}
+
+function Rotate(props: JSX.IntrinsicElements['group']){
+  const ref = useRef<Group>(null!);
+
+  useFrame((state) => {
+    if (ref.current) {
+      ref.current.rotation.y = state.clock.elapsedTime;
+    }
+  });
+
+  return <group ref={ref} {...props} />;
+};
+  
+function Model({ url, ...props }: { url: string }) {
+    // useGLTF suspends the component, it literally stops processing
+    const { scene } = useGLTF(url)
+    // By the time we're here the model is guaranteed to be available
+    return <primitive object={scene} {...props} />
+}
+
+export default function App(){
+    return (
+        <Suspense fallback={<span>loading...</span>}>
+            <Canvas dpr={[1, 2]} camera={{ position: [-2, 2, 4], fov: 25 }}>
+            
+            <Rotate position-y={0.01} position-x={0.01} scale={0.2}>
+                <Suspense fallback={<Model url="/cube.gltf" />}>
+                    <Model url="/cube.gltf" />
+                </Suspense>
+            </Rotate>
+            <ambientLight intensity={0.1} />
+            <directionalLight position={[0, 0, 5]} color="red" />
+        
+            </Canvas>
+        </Suspense>
+    )
+}
+
+// export default ThreeScene;
