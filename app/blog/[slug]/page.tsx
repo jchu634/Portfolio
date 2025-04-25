f25a94740d7aabac5db87e81273971cabd93f220
Joshua Chung <87676426+jchu634@users.noreply.github.com>
2025-04-25 17:07:41 +1200

    Add-Licensing (#24)
    * Added Licenses to codebase
    
    * Added Creative commons notice to blogposts
    
    * Fixed issue where <ul> was under <p> in blog 2

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { roboto_slab } from "@/lib/fonts";
 import { cn } from "@/lib/utils";
 import { notFound } from "next/navigation";
+import { Separator } from "@/components/ui/separator";
+import Image from "next/image";
+import Link from "next/link";
 
 export default async function BlogPost({
   params,
@@ -20,6 +23,30 @@ export default async function BlogPost({
     return (
       <div className={cn("h-full w-full p-10", roboto_slab.className)}>
         <PostContent />
+        <div className="prose lg:prose-xl dark:prose-invert prose-p:text-zinc-800 dark:prose-p:text-zinc-200 prose-a:text-blue-600 dark:prose-a:text-blue-400 max-w-none">
+          <Separator className="bg-black dark:bg-white"></Separator>
+
+          <div className="flex flex-row items-center justify-end text-right">
+            This work is licensed under{" "}
+            <Link
+              href="https://creativecommons.org/licenses/by/4.0/?ref=chooser-v1"
+              className="flex h-16 flex-row items-center space-x-2 pl-2"
+            >
+              <p>CC BY 4.0</p>
+
+              <Image
+                src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1"
+                className="size-8"
+                alt="Creative Commons Icon"
+              />
+              <Image
+                src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1"
+                className="size-8"
+                alt="Creative Commons BY Icon"
+              />
+            </Link>
+          </div>
+        </div>
       </div>
     );
   } catch (error) {
