9915d3e173bd2667c5f659888332314c5e99578c
JCHU634 <87676426+jchu634@users.noreply.github.com>
2025-05-23 02:01:20 +1200

    Changed mobile padding to 20px

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
         >
           <div className="flex h-screen flex-col-reverse bg-blue-200 md:flex-row dark:bg-slate-900">
             <SideNav />
-            <div className="flex-grow overflow-y-auto pb-15 md:pb-0">
+            <div className="flex-grow overflow-y-auto pb-20 md:pb-0">
               {children}
             </div>
           </div>
