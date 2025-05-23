1b4a0bfde650b78a4649e6eb2ec88df6e6c92bcc
JCHU634 <87676426+jchu634@users.noreply.github.com>
2025-05-23 21:56:16 +1200

    Added test page background

---
+++ app/test/page.tsx
@@ -0,0 +1,28 @@
+export default function test() {
+  return (
+    <div
+      className="h-full w-full"
+      style={{
+        background: `repeating-conic-gradient(
+          from 0deg at top right,
+          #3B82F6 0deg 22.5deg,
+          #8B5CF6 22.5deg 45deg,
+          #EC4899 45deg 67.5deg,
+          #F97316 67.5deg 90deg,
+          #EAB308 90deg 112.5deg,
+          #22C55E 112.5deg 135deg,
+          #06B6D4 135deg 157.5deg,
+          #6366F1 157.5deg 180deg,
+          #3B82F6 180deg 202.5deg,
+          #8B5CF6 202.5deg 225deg,
+          #EC4899 225deg 247.5deg,
+          #F97316 247.5deg 270deg,
+          #EAB308 270deg 292.5deg,
+          #22C55E 292.5deg 315deg,
+          #06B6D4 315deg 337.5deg,
+          #6366F1 337.5deg 360deg
+        )`,
+      }}
+    ></div>
+  );
+}
