ad1e1e811939d472747bc24cafbdd16fa2a8c015
JCHU634 <87676426+jchu634@users.noreply.github.com>
2025-07-12 23:04:23 +1200

    Rewrote blog 7

--- blogposts/00007.OPFS.tsx
+++ blogposts/00007.OPFS.tsx
@@ -3,11 +3,18 @@ import BlogHeader from "@/components/ui/blog-header";
 import { CodeBlock } from "@/components/ui/codeblock";
 import { intel_one_mono } from "@/lib/fonts";
 import { cn } from "@/lib/utils";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTriggerShell,
+} from "@/components/ui/accordion";
+import { ChevronDownIcon } from "lucide-react";
 
 export const metadata: Metadata = {
-  title: "OPFS Extension Limitations in Firefox",
+  title: "Documenting the OPFS Browser development",
   date: "2025-07-10",
-  description: "AKA: How is a user supposed to access OPFS files in Firefox.",
+  description: "This wasn't supposed to work...",
   lastUpdate: "2025-07-10",
 };
 export default function Post() {
@@ -31,7 +38,6 @@ export default function Post() {
   return (
     <article className="prose lg:prose-xl dark:prose-invert prose-headings:text-zinc-900 dark:prose-headings:text-zinc-100 prose-p:text-zinc-800 dark:prose-p:text-zinc-200 prose-blockquote:border-zinc-700 dark:prose-blockquote:border-zinc-200 prose-blockquote:border-l-4 prose-blockquote:pl-4 prose-blockquote:text-zinc-700 dark:prose-blockquote:text-zinc-300 prose-li:text-zinc-800 dark:prose-li:text-zinc-200 prose-li:marker:text-zinc-600 dark:prose-li:marker:text-zinc-400 prose-ul:list-disc prose-ol:list-decimal prose-a:text-blue-600 dark:prose-a:text-blue-400 max-w-none pb-10">
       <BlogHeader header_metadata={metadata} />
-
       <div>{/* Content goes here */}</div>
       <div>
         <h3>Background</h3>
@@ -39,60 +45,65 @@ export default function Post() {
           For the Bolt.new hackathon, I worked on{" "}
           <a href="inkproof.keshuac.com">Inkproof</a>, a epub editor which
           stores all project info in the Origin Private File System (OPFS).
-          <br />I personally use Zen as my browser of choice despite my issues
-          with how Firefox handles large amounts of tabs.
-          <br />
-          However, I was unable to find a good way to debug Inkproof in Zen as I
-          could not browse the OPFS structure for debugging. so I used Vivaldi
-          to debug using the{" "}
-          <a href="https://github.com/tomayac/opfs-explorer">
-            OPFS Explorer
-          </a>{" "}
-          extension.
+          <br />I was unable to debug it easily in Zen, so I decided I wanted to
+          port over or make my own extension to debug OPFS.
         </p>
         <p>
-          After the hackathon, I was confused to why there aren't any extensions
-          to browse the OPFS in Firefox-based browsers, and so I decided to try
-          port over an extension to Firefox.
+          That is where my things diverge from my original draft of this
+          article.
           <br />
-          I am now here to announce, after a lot of work...
-          <br />I failed, I was unable to get any OPFS extensions to work in
-          Firefox and this is why.
+          I was originally stuck and unable to get anything to work, but I
+          decided to try one more extension API, and it worked.
+          <br />
+          So this article is an attempt to salvage the content from my original
+          blogpost and to document what in the OPFS works or doesn't on Firefox
+          Addons.
         </p>
       </div>
       <div>
         <h3>Extensions 101</h3>
-        <blockquote>Necessary context on how Extensions work</blockquote>
-        <p>
-          In the browers extension world, you can register HTML and JS to run in
-          certain contexts: (background, content, devtools, newtab, options,
-          panel, popup).
-          <br />
-          For this article, the only contexts that are relevant are Background,
-          Content and Devtools.
-          <br />
-        </p>
-        <p>
-          Background Scripts as implied, run in the background, their job is to
-          monitor and respond to browser events.
-          <br />
-          e.g. there might be a background script which only triggers when you
-          open a new tab.
-          <br />
-          Background scripts can either be ethereal and only loaded when an
-          event happens or they can be persistent and run the entire time the
-          browser has loaded a certain extension.
-          <br />
-        </p>
-        <p>
-          Content Scripts are intended to run on a webpage, they are used as
-          they are able to read and modify the contents of a page. Hence Content
-          Scripts are immensely powerful.
-        </p>
-        <p>
-          Devtool Scripts are incredibly specialised, intended to run on a page
-          in the browser's devtools.
-        </p>
+        <blockquote>Optional context on how Extensions work</blockquote>
+        <Accordion type="single" collapsible className="not-prose">
+          <AccordionItem value="item-1" className="prose-pre:m-0">
+            <AccordionTriggerShell className="text-2xl font-semibold">
+              Content (Click to show)
+              <ChevronDownIcon />
+            </AccordionTriggerShell>
+
+            <AccordionContent className="prose-pre:m-0 space-y-2 text-xl/9">
+              <p>
+                In the browers extension world, you can register HTML and JS to
+                run in certain contexts: (background, content, devtools, newtab,
+                options, panel, popup).
+                <br />
+                For this article, the only contexts that are relevant are
+                Background, Content and Devtools.
+                <br />
+              </p>
+              <p>
+                Background Scripts as implied, run in the background, their job
+                is to monitor and respond to browser events.
+                <br />
+                e.g. there might be a background script which only triggers when
+                you open a new tab.
+                <br />
+                Background scripts can either be ethereal and only loaded when
+                an event happens or they can be persistent and run the entire
+                time the browser has loaded a certain extension.
+                <br />
+              </p>
+              <p>
+                Content Scripts are intended to run on a webpage, they are used
+                as they are able to read and modify the contents of a page.
+                Hence Content Scripts are immensely powerful.
+              </p>
+              <p>
+                Devtool Scripts are incredibly specialised, intended to run on a
+                page in the browser's devtools.
+              </p>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
       </div>
       <div>
         <h3>Existing Solutions</h3>
@@ -151,7 +162,7 @@ export default function Post() {
         </div>
       </div>
       <div>
-        <h3>Why nothing works on Firefox</h3>
+        <h3>Why these don't work on Firefox</h3>
         <blockquote>
           Note: Firefox uses "browser" and not "chrome" as their api keyword,
           the rest of the article will reflect this
@@ -179,7 +190,7 @@ export default function Post() {
           You would think that OPFS-Explorer would then be fine, as it does its
           OPFS calls from the correct context through bridge.
           <br />
-          But it breaks in an entirely different and frustrating way.
+          But it breaks in an entirely different way.
           <br />
         </p>
         <p>
@@ -196,16 +207,15 @@ export default function Post() {
           api which is needed to get the directory structure is broken.
           <br />
           On Firefox, this fetches an empty iterable which triggers an error.
-          <CodeBlock hideCopyButton={true}>
-            directoryHandle.values() is not iterable (TypeError)
-          </CodeBlock>
         </p>
+        <CodeBlock hideCopyButton={true}>
+          directoryHandle.values() is not iterable (TypeError)
+        </CodeBlock>
         <p>
-          Through this, Firefox appears to have deliberately or unintentionally
-          locked down the ability for extensions to access the OPFS directory
-          listings as the other FileSystemDirectoryHandle APIS appear work fine
-          (read, write, delete) and you can even check if a particular file
-          exists through{" "}
+          Through this, Firefox appears to have no way for most extensions to
+          access the OPFS directory listings as the other
+          FileSystemDirectoryHandle APIS appear work fine (read, write, delete)
+          and you can even check if a particular file exists through{" "}
           <code
             className={cn(
               "rounded-md bg-slate-800 p-1 text-white",
@@ -217,38 +227,105 @@ export default function Post() {
         </p>
       </div>
       <div>
-        <h3>Conclusions</h3>
-        In the end, it appears that Firefox has specifically locked down the
-        ability to map the OPFS from extensions. As{" "}
-        <code
-          className={cn(
-            "rounded-md bg-slate-800 p-1 text-white",
-            intel_one_mono.className,
-          )}
-        >
-          FileSystemDirectoryHandle.values()
-        </code>{" "}
-        works fine when using this script from{" "}
-        <a href="https://shaneosullivan.wordpress.com/2023/11/28/how-to-list-all-files-in-a-browsers-origin-private-file-system/">
-          Shane O'Sullivan's blog.
-        </a>
-        <a
-          href="
-            https://shaneosullivan.wordpress.com/2023/11/28/how-to-list-all-files-in-a-browsers-origin-private-file-system/"
-        ></a>
-        <CodeBlock>{code}</CodeBlock>
+        <h3>My Approach</h3>
         <p>
-          However, this script is fundamentally limited, as it needs to be
-          re-run if you want to see latest state of the OPFS and it only shows
-          you the OPFS structure, you cannot download, upload or modify the OPFS
-          files.
+          So if the most important OPFS apis are locked down from being called
+          in extensions, how did I get it to work.
+        </p>
+        <CodeBlock hideCopyButton={true}>
+          devtools.inspectedWindow.eval() // Referred to as eval() from now on
+          eval()
+        </CodeBlock>
+        <p>
+          <code
+            className={cn(
+              "rounded-md bg-slate-800 p-1 text-white",
+              intel_one_mono.className,
+            )}
+          >
+            eval()
+          </code>{" "}
+          acts very simimilarly to{" "}
+          <code
+            className={cn(
+              "rounded-md bg-slate-800 p-1 text-white",
+              intel_one_mono.className,
+            )}
+          >
+            tabs.executeScript()
+          </code>
+          , but most importantly it reads the current state of the page, unlike
+          Content-Scripts which are isolated from changes made to the page by
+          page scripts.
           <br />
-          And to that end, it appears that there will not be a good way to do so
-          in Firefox unless the browser developers add a native way to do so in
-          the devtools.
+          This is most likely the reason why{" "}
+          <code
+            className={cn(
+              "rounded-md bg-slate-800 p-1 text-white",
+              intel_one_mono.className,
+            )}
+          >
+            eval()
+          </code>{" "}
+          is uniquely able to access the OPFS correctly.
+        </p>
+        <p>
+          However, an issue is that any script run by
+          <code
+            className={cn(
+              "rounded-md bg-slate-800 p-1 text-white",
+              intel_one_mono.className,
+            )}
+          >
+            eval()
+          </code>{" "}
+          must return to a JSON representable value.
           <br />
-          So it appears that I will be returning to using Vivaldi a bit longer,
-          until I can finally debug Inkproof.
+          This caused all sorts of headache, as the OPFS api is asynchronous and
+          would return a promise of JSON, which I could not get{" "}
+          <code
+            className={cn(
+              "rounded-md bg-slate-800 p-1 text-white",
+              intel_one_mono.className,
+            )}
+          >
+            eval()
+          </code>{" "}
+          to resolve at all.
+          <br />
+          Ultimately, my solution was to give up on a clean Devtools only script
+          and to architect the extension so that
+        </p>
+        <ol>
+          <li>
+            Devpanel executes OPFS commands via{" "}
+            <code
+              className={cn(
+                "rounded-md bg-slate-800 p-1 text-white",
+                intel_one_mono.className,
+              )}
+            >
+              eval()
+            </code>
+          </li>
+          <li>OPFS Results are written to an element in the DOM</li>
+          <li>
+            A Content-Script picks up the results and sends it to the background
+            script
+          </li>
+          <li>The background script forwards the results to the Devpanel</li>
+        </ol>
+      </div>
+      <div>
+        <h3>Conclusions</h3>
+        <p>
+          Although I wish I could have gotten the devpanels only architecture
+          working, ultimately the extension does work. <br />
+          Overall, the native Devtools should really get some native OPFS
+          capabilities, but I don't think that is coming anytime soon.
+          <br />
+          Here's hoping that day will come sooner than later, but until then,
+          3rd-party extensions work fine enough.
         </p>
       </div>
     </article>
