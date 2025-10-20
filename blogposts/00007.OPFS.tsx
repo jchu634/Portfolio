8a7b3c7481d8b8d484c6b986fcba9089cbcbab1e
JCHU634 <87676426+jchu634@users.noreply.github.com>
2025-10-21 02:14:11 +1300

    Removed placeholder div in blog 7

--- blogposts/00007.OPFS.tsx
+++ blogposts/00007.OPFS.tsx
@@ -39,7 +39,6 @@ export default function Post() {
   return (
     <article className="prose lg:prose-xl dark:prose-invert prose-headings:text-zinc-900 dark:prose-headings:text-zinc-100 prose-p:text-zinc-800 dark:prose-p:text-zinc-200 prose-blockquote:border-zinc-700 dark:prose-blockquote:border-zinc-200 prose-blockquote:border-l-4 prose-blockquote:pl-4 prose-blockquote:text-zinc-700 dark:prose-blockquote:text-zinc-300 prose-li:text-zinc-800 dark:prose-li:text-zinc-200 prose-li:marker:text-zinc-600 dark:prose-li:marker:text-zinc-400 prose-ul:list-disc prose-ol:list-decimal prose-a:text-blue-600 dark:prose-a:text-blue-400 max-w-none pb-10">
       <BlogHeader header_metadata={metadata} />
-      <div>{/* Content goes here */}</div>
       <div>
         <h3>Background</h3>
         <p>
