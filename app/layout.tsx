126a98020d2b9fef4d267b3eb0b057e763c85d8b
JCHU634 <87676426+jchu634@users.noreply.github.com>
2025-10-29 02:57:24 +1300

    Added Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { ThemeProvider } from "@/components/theme-provider";
 import SideNav from "@/components/sidenav";
 import MobileFooter from "@/components/mobile-footer";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -28,6 +29,7 @@ export default function RootLayout({
             <SideNav />
             <div className="flex-grow overflow-y-auto pb-15 md:pb-0">
               {children}
+              <Analytics />
             </div>
           </div>
           <MobileFooter />
