1323fae67fcec01f9204cfc93250d3fb80c2c8c3
Joshua Chung <87676426+jchu634@users.noreply.github.com>
2026-06-15 12:47:23 +1000

    EdGo PT1 minor rewrite

--- blogposts/00014.edgo-writeup-pt1.tsx
+++ blogposts/00014.edgo-writeup-pt1.tsx
@@ -5,7 +5,7 @@ export const metadata: Metadata = {
   title: "EdGo Writeup Pt 1.",
   date: "2026-06-05",
   description: "Agentic Coding with Non-Frontier Models",
-  lastUpdate: "2026-06-05",
+  lastUpdate: "2026-06-15",
 };
 export default function Post() {
   return (
@@ -28,39 +28,37 @@ export default function Post() {
         were used to help.
       </div>
       <div>
-        <h2>LLM (AI) Usage</h2>
+        <h3>LLM (AI) Usage</h3>
         Lets get the elephant in the room out of the way first, I will cover the
         technical details of EdGo in part 2.
         <br />
-        LLMs were heavily used in this project, but this was NOT vibecoded.
+        Although LLMs were heavily used in this project, this was not vibecoded,
+        as most of the raw output needed heavy alterations to truly integrate
+        into the codebase.
         <br />
-        Additionally, none of my blogposts are written with AI.
+        The rest of this blogpost will be documenting my workflow at this point,
+        as I thought this may be interesting to document my workflow at this
+        point for future reference when models inevitably get better.
         <br />
-        However, I though it would be interesting to document how I used the
-        models at this point.
         <br />
+        Firstly my workflow is structured around a couple of things:
+        <h4>1. Skill Level</h4>
+        Currently, I would say that I can generally understand the generated
+        code, and can determine when it is bad and can fix/debug the code. But I
+        don't quite understand the systems enough to write the features myself
+        without significant effort, hence the use of LLMs as a POC reference
+        implementation and a learning scaffold.
         <br />
-        Firstly my workflow is built around a couple of things:
-        <h4>My Skill Level</h4>
-        Currently, I would self-classify my skills as a junior engineer who
-        knows too much.
-        <br />
-        What this meant is that I can understand enough to determine when a LLM
-        solution is bad, and I can debug broken code semi-effectively, but I
-        still need the LLM as a slightly unreliable learning aid/assistant.
-        <br />
-        <h4>The Models</h4>
-        My workflow was adapted to the limitations of the models I was using.
-        <br />
+        <h4>2. The Models</h4>
         There were four models used thoughout development (April - June 2026),
         which changed as new models released.
         <ol>
           <li>
             GLM Family (GLM 4.7 + GLM 5.1)
             <br />
-            The workhorses of this project, they were generally competant but
-            notably had quite a few issues with complex technical tasks and
-            required quite a bit of steering for Effect-ts code.
+            The workhorses of this project, worked well, but notably had quite a
+            few issues with complex technical tasks and required quite a bit of
+            steering for Effect-ts code.
             <br />
             <div className="font-bold">
               Why GLM and not any other model? (GPT Series, Kimi, Minimax etc.)
@@ -71,20 +69,21 @@ export default function Post() {
           <li>
             GPT 5.3 Codex
             <br />
-            Exceptionally capable, but since I had limited requests, I rationed
-            its use for more difficult technical tasks.
+            Wrote much better functional code, but since I had limited requests,
+            I generally rationed its use for more difficult technical tasks.
             <br />
-            About halfway through the project, I had to switch over to GLM-5.1
-            entirely as Github Student plans removed GPT 5.3 Codex access.
+            About halfway through the project in April, I had to switch over to
+            GLM-5.1 entirely as Github Student plans removed GPT 5.3 Codex
+            access entirely.
           </li>
           <li>
             GPT 5.4
             <br />
-            It was used to refine architecture decisions and to critique
-            functions given to it.
+            Used to refine architecture decisions and to critique functions
+            given to it.
             <br />
-            I would have loved to use it in programming, but I could only chat
-            to it through my t3.chat subscription.
+            I had to use other models for general programming, as this was used
+            through my chat subscription (t3.chat).
             <br />
           </li>
         </ol>
@@ -93,8 +92,9 @@ export default function Post() {
         <br />
         <ul>
           <li>
-            Zed Agents were perfect for minor fixes and tweaks, (e.g. Change all
-            the button colours to use another colour scheme)
+            Zed Agents were perfect for minor fixes and tweaks, but was not
+            chosen for more complex features as it wrote notably worse code than
+            OpenCode with the same model (GLM family).
           </li>
           <li>
             OpenCode was used for creating more complicated features as its
@@ -102,53 +102,58 @@ export default function Post() {
             iterate on its own.
           </li>
         </ul>
-        <h4>The Workflow</h4>
-        What is most interesting is probably the workflow, which may differ from
-        others due to the heavy use of GLM-5.1.
+        I suspect Zed Agents underperformed with the GLM models because its
+        system prompt made it use tools much less, leading to a lack of
+        "understanding" of the general codebase in its context, which caused it
+        to write worse code.
+        <h4>The Workflow (Complex Features)</h4>
+        If I didn't have a good concept of the technical pinnings of a feature,
+        the initial "spec" creation would be done with GPT-5.4, or rather a high
+        level concept with some implementation specifics (e.g. I want ___ to be
+        written to the DB, also create a Drizzle and Effect schema.)
         <br />
-        Although GLM-5.1 has no problems with simple feature requests, it tended
-        to struggle with oneshotting complex technical features.
+        This was necessary as although GLM-5.1 has no problems with simple
+        feature requests, it generally has bad "taste" and struggles with
+        complex technical features. GLM 5.1 would be then tasked to create a
+        plan to implement that feature and with a reference to the important
+        files.
         <br />
-        Hence I would initially consult GPT-5.4 and create a high level overview
-        of the plan.
-        <br />
-        GLM 5.1 would be then tasked to create a plan to implement that feature,
-        and is given a reference to the most important file/s.
-        <br />
-        I found that in prompting, in general, it was important to not over or
-        understeer the instructions, as it needed a certain balance between not
-        making stupid decisions and not following my stupid decisions.
-        <br />
-        Then there might be a couple of back+forths to refine the plan so that
-        it wouldn't do something stupid.
-        <br />
-        Finally, it would be set to build and told to implement. This works
-        quite well although there be couple of tweaks needed to fix edge cases.
+        <blockquote>
+          I found that it was important to not over/understeer GLM 5.1 when
+          prompting it as it needed a certain balance between not overfitting to
+          my instructions when it lead to a worse implementation, and not making
+          stupid decisions itself.
+        </blockquote>
+        Then there might be a couple of back+forths to refine the plan before it
+        would be set to build and told to implement. This generally worked quite
+        well after a couple of tweaks needed to fix edge cases.
         <br />
         <br />
-        Occasionally, GLM would entirely fall on its face, these often happened
-        as it found some shite code in the codebase and would proceed to use
-        that as a reference or on particularly complex tasks where more guidance
-        would be needed.
+        Occasionally, GLM would entirely fall on its face, this was often
+        because it would find a bad piece of code in the codebase which would
+        contaminate its context as it would proceed to use that as a reference.
         <br />
         The only solution then was to revert all changes and create a new thread
-        with fresh context and be more thorough in its initial prompt or to
-        explicitly tell it that particular source files were an anti-pattern.
+        with fresh context and be more thorough in its initial prompt to avoid
+        that pattern.
+        <br />
+        What is notable is that my workflow GPT-5.3 Codex did not have most of
+        these issues, and in my experience gave equal or better results with
+        less steering.
       </div>
       <div>
         <h2>Conclusion</h2>
-        GLM-5.1 is a exceptionally capable model compared to the early days of
-        AI, however it is still lacking in certain ways that proprietary models
-        by frontier labs aren't. <br />
-        Although this is a fundamentally worse experience, I would say that is
-        likely better at letting me improve my skills. As each solution it
-        creates requires a code review to find mistakes or edge cases it forgot
-        to handle which is great for learning.
+        Open models like GLM-5.1 are exceptionally capable, especially compared
+        to the early GPT-3 days, however they are still lacking in certain ways
+        that proprietary models by frontier labs aren't. <br />
+        Although the experience is much worse, I would say that is better for
+        developping my skills and understanding as each solution requires a
+        thorough code review to find mistakes or edge cases it forgot to handle.
         <br />
         Overall I would say that open models tend to more heavily embody "
         garbage in garbage out", where the quality of its output is more
         dependant on the skills of the user, whereas more frontier models can
-        better code with less skills.
+        generate better code with less skills.
         <br />
         Now that the AI writeup is out of the way, the next blogpost will be a
         bit more fun as it takes on the technical stuff.
